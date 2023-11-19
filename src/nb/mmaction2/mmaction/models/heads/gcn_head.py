9f23c8a0adccf7346b503a0f8a402bb5c7832bfc
KooroshRH <korosh.roohi9731@gmail.com>
2023-11-19 16:52:26 -0500

    Add dropout

--- src/nb/mmaction2/mmaction/models/heads/gcn_head.py
+++ src/nb/mmaction2/mmaction/models/heads/gcn_head.py
@@ -44,9 +44,9 @@ class GCNHead(BaseHead):
         else:
             self.dropout = None
 
+        self.dropout = nn.Dropout(p=0.2)
         self.pool = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Linear(self.in_channels, self.num_classes)
-        self.sig = nn.Sigmoid()
 
     def forward(self, x: torch.Tensor, **kwargs) -> torch.Tensor:
         """Forward features from the upstream network.
@@ -69,5 +69,4 @@ class GCNHead(BaseHead):
             x = self.dropout(x)
 
         cls_scores = self.fc(x)
-        cls_scores = self.sig(cls_scores)
         return cls_scores
